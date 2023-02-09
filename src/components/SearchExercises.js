1031a34fd6bc27a6fa8d3aa2cb69d9118aec2ecc
EnxhiHazisllari <enxhi.hazisllari14@gmail.com>
2023-02-09 18:35:44 +0100

    Add HorizontalScrollbar

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -2,11 +2,12 @@ import React, {useState, useEffect} from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
 const SearchExercises = () => {
 
-    const [ search, setSearch ] = useState('');
-    const [ exercises, setExercises ] = useState([]);
+    const [search, setSearch ] = useState('');
+    const [exercises, setExercises ] = useState([]);
     const [bodyParts, setBodyParts] = useState([]);
 
     useEffect(()=>{
@@ -74,6 +75,7 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
+        <HorizontalScrollbar data={bodyParts}/>
       </Box>
     </Stack>
   )
