f02c48ade31d7d2ab1a86c221893eff03cd127bb
EnxhiHazisllari <enxhi.hazisllari14@gmail.com>
2023-02-21 14:21:42 +0100

    Update SimilarExercises

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { Box, Typography } from '@mui/material';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import ExerciseCard from './ExerciseCard';
 import BodyPart from './BodyPart';
 import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
@@ -25,18 +26,21 @@ const RightArrow = () => {
   );
 };
 
-const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart, isBodyParts }) => {
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-        {data.map((item)=> 
+        {data.map((item)=> (
         <Box 
         key={item.id || item}
         ItemId={item.id || item}
         title={item.id || item}
         m="0 40px"
         >
-          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />              
-        </Box>)}
+          {isBodyParts ? <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+          : <ExerciseCard exercise={item}/>}          
+        </Box>
+          )
+        )}
     </ScrollMenu>
   )
 }
